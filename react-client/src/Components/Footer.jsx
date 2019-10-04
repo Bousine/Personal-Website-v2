6c712436d68090f78f633908bf13b560a1d34b16
Bousine <asif21hasan@gmail.com>
2019-10-04 19:33:15 -0400

    Added footer links

--- react-client/src/Components/Footer.jsx
+++ react-client/src/Components/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import {Navbar} from 'react-bootstrap'
+import FooterIcon from './FooterIcon'
+import Icon from './icons'
 
 function Footer(){
   return(
-    <footer>
-      FOOTER
+    <footer id="footer">
+      <div className="icon"><a href="https://www.linkedin.com/in/asif-hasan-063b9b149/" target="_blank"><Icon name="linkedin" width={50} fill="#000000"/></a></div>
+      <div className="icon"><a href="https://twitter.com/AsifBousine" target="_blank"><Icon name="twitter" width={50} fill="#000000"/></a></div>
+      <div className="icon"><a href="mailto:asif21hasan@gmail.com" target="_blank"><Icon name="gmail" width={50} fill="#000000"/></a></div>
+      <div className="icon"><a href="https://github.com/Bousine" target="_blank"><Icon name="github" width={50} fill="#000000"/></a></div>
     </footer>
   )
 }
