4b38e0865a69eee9deac7e779e5eeeac1cffac3b
Bousine <asif21hasan@gmail.com>
2019-10-11 23:20:21 -0400

    Fixed the resume pdf link and added max-width to css.

--- react-client/src/Components/Header.jsx
+++ react-client/src/Components/Header.jsx
@@ -13,7 +13,7 @@ function Header(){
           <Nav id="navbuttons" className="mr-auto justify-content-center">
             <Nav.Link href="#home" active>Home</Nav.Link>
             <Nav.Link href="https://github.com/Bousine" target="_blank">Projects</Nav.Link>
-            <Nav.Link href="../../dist/assets/Resume.pdf" download>Resume</Nav.Link>
+            <Nav.Link href="https://bit.ly/2p96env" target="_blank">Resume</Nav.Link>
           </Nav>
         </Navbar.Collapse>
         <Navbar.Brand id="brandname">Asif Hasan</Navbar.Brand>
