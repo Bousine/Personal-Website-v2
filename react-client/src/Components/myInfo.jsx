19862591c6d0437579c5364041b0193bb838e72f
Bousine <asif21hasan@gmail.com>
2020-05-20 18:19:02 -0400

    updated info

--- react-client/src/Components/myInfo.jsx
+++ react-client/src/Components/myInfo.jsx
@@ -1,9 +1,9 @@
 const myInfo={
   title: "Asif Hasan",
   subtitle: "Coder. Designer. Developer.",
-  firstp: "Hello visitor! My name is Asif Hasan. I am a senior in Computer Science B.S. at UB. I am from Dhaka, Bangladesh, and now in the United States to pursue higher education. Modern technology is totally ingrained in computer science, so the possibilities are endless when it comes to learning technologies in this field. My interest mainly lies in front-end development and graphic design. The journey so far has not been easy but it was definitely rewarding. Perhaps I will further pursue an M.S. in Computer Science after some time. For now, I only seek to be a competent software engineer.",
-  secondp: "When it comes to my hobbies, I am mostly interested in video games and movies. They are obviously great sources of entertainment but the level of creativity depicted in video games and movies is honestly quite amazing and inspiring. I am also deeply invested in watching soccer. Otherwise, travelling abroad is what I would like to do more often. The world is a beautiful place waiting to be explored.",
-  thirdp: "Also, don't forget to check out my projects!"
+  firstp: "Hello visitor! My name is Asif Hasan. I am a full-stack developer based in New York. I graduated from University at Buffalo in Computer Science. I love to innovate. Since there is infinite room for innovation in this field, I love developing software. My interest mainly lies in app development and UI design. The journey so far has not been easy but it was definitely rewarding. Perhaps I will further pursue an M.S. in Computer Science after some time. Right now, I am working freelance and looking for a full time job as well.",
+  secondp: "When it comes to my hobbies, I like playing video games and watching movies. They are obviously great sources of entertainment but the level of creativity depicted in video games and movies is honestly very inspiring to me. I am also deeply invested in watching soccer. Otherwise, travelling abroad is what I would like to do more often. The world is a beautiful place waiting to be explored.",
+  thirdp: "Feel free to contact me if you need any help with a project."
 }
 
 export default myInfo
