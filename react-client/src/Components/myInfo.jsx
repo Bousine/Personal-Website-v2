332bbea3b0c87db9c2eff6a6a64a31ec49c4aad5
Alif boktear <asif21hasan@gmail.com>
2020-05-28 00:32:20 -0400

    Updated myinfo

--- react-client/src/Components/myInfo.jsx
+++ react-client/src/Components/myInfo.jsx
@@ -1,7 +1,7 @@
 const myInfo={
   title: "Asif Hasan",
   subtitle: "Coder. Designer. Developer.",
-  firstp: "Hello visitor! My name is Asif Hasan. I am a full-stack developer based in New York. I graduated from University at Buffalo in Computer Science. I love to innovate. Since there is infinite room for innovation in this field, I love developing software. I love web technologies like React and Node, and spend a huge chunk of my time working on web projects. I am obsessed about optimizing the runtime within my apps and making the UI as user-friendly and intuitive as possible, because what's the point of software if the user is not having a good time using it?",
+  firstp: "Hello visitor! My name is Asif Hasan. I am a full-stack developer based in New York. I graduated from University at Buffalo in Computer Science. I love to innovate. Since there is infinite room for innovation in this field, I love developing software. I love web technologies like React and Node, and spend a huge chunk of my time working on web projects. I am obsessed with optimizing the runtime within my apps and making the UI as user-friendly and intuitive as possible, because what's the point of software if the user is not having a good time using it?",
   secondp: "When it comes to my hobbies, I like playing video games and watching movies. They are obviously great sources of entertainment but the level of creativity depicted in video games and movies is honestly very inspiring to me. Otherwise, travelling abroad is something that I would like to be able to do more often. The world is a beautiful place waiting to be explored.",
   thirdp: "Feel free to contact me if you need any help with a project."
 }
